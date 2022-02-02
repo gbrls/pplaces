650d01a31ece564c0b3d6ac85112456eacce0297
Gabriel Schneider <gas6@cin.ufpe.br>
2022-02-02 16:06:23 -0300

    show and scan done

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 #![feature(type_alias_impl_trait)]
 
-use std::{collections::HashMap, fs, io::Write, path::Path, process::Command};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    process::Command,
+};
 
-use chrono::{NaiveDate, NaiveDateTime};
+use chrono::{DateTime, Duration, Local, NaiveDate, NaiveDateTime, TimeZone, Utc};
 use clap::Parser;
-
-use std::io::{self};
 
 type Cache = Vec<ProjectMetadata>;
 
-#[derive(Parser, Debug)]
+#[derive(Serialize, Deserialize, Debug, Parser)]
 enum CmdType {
     /// Recursively look for git repositories in given path
     Scan { path: String },
     /// Wrapper around git clone to check if the repo is already cloned
-    Clone { url: String },
+    Clone { args: Vec<String> },
     /// Show all git repos with some metadata
     Show,
 }
@@ -25,14 +28,20 @@ enum CmdType {
 struct CliArgs {
     #[clap(subcommand)]
     cmd_type: CmdType,
+    #[clap(short, long, default_value_t = 7u32)]
+    days_to_show: u32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct ProjectMetadata {
     path: String,
     upstream: Vec<String>,
     latest_commit: Option<NaiveDateTime>,
     //latest_modification:
+}
+
+struct Config {
+    paths: bool,
 }
 
 fn scan(path: &Path, cache: &mut Cache) {
@@ -48,7 +57,14 @@ fn scan(path: &Path, cache: &mut Cache) {
     }
 }
 
-fn clone(_url: &str) {}
+fn clone(args: &Vec<String>, data: &Cache) {
+    println!("{args:#?}");
+
+    let url = args
+        .iter()
+        .find(|s| s.starts_with("http") || s.starts_with("git@"));
+    println!("{url:?}");
+}
 
 fn process_repo(path: &Path, cache: &mut Cache) {
     // We assume that there won't be repetition, so a Vec is just fine.
@@ -127,9 +143,67 @@ fn build_cache(path: &Path) -> Cache {
     let mut data: Cache = Vec::new();
     scan(path, &mut data);
     data.sort_by_key(|d| d.latest_commit);
+    data.reverse();
 
     data
 }
+
+fn config_dir() -> Option<PathBuf> {
+    if let Some(config_dir) = dirs::config_dir() {
+        let config_dir = config_dir.join("pplaces");
+        Some(config_dir)
+    } else {
+        None
+    }
+}
+
+fn save_cache_to_disk(cache: &Cache) {
+    if let Some(config_dir) = config_dir() {
+        fs::create_dir_all(&config_dir).unwrap();
+
+        // this is written as a JSON because it's easier to interface with web technologies
+        let str = serde_json::to_string(cache).unwrap();
+        fs::write(config_dir.join(".cache.json"), &str).unwrap();
+    }
+
+    // We don't have an else because it should work even without a disk cache.
+}
+
+fn get_cache_from_disk() -> Result<Cache, Box<dyn std::error::Error>> {
+    let data_str = fs::read_to_string(config_dir().unwrap().join(".cache.json"))?;
+    let data = serde_json::from_str::<Cache>(&data_str)?;
+
+    Ok(data)
+}
+
+fn print_paths(data: &Cache) {
+    for entry in data {
+        println!("{}", entry.path);
+    }
+}
+
+fn print_recent(data: &Cache, since: Duration) {
+    for entry in data.iter().filter(|e| {
+        if e.latest_commit.is_some() {
+            if let Some(date_time) = e.latest_commit {
+                let date_time: DateTime<Local> = Local
+                    .from_local_datetime(&e.latest_commit.unwrap())
+                    .unwrap();
+                let elapsed = Local::now() - date_time;
+
+                elapsed <= since
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    }) {
+        println!("{}", entry.path);
+    }
+}
+
+// https://stackoverflow.com/questions/2423777/is-it-possible-to-create-a-remote-repo-on-github-from-the-cli-without-opening-br
 
 fn main() {
     let args = CliArgs::parse();
@@ -140,14 +214,21 @@ fn main() {
             if !path.is_dir() {
                 panic!("{path:?} is not a directory");
             }
+            // This might be slow in some machines
             let data = build_cache(path);
+            save_cache_to_disk(&data);
+            print_recent(&data, Duration::days(args.days_to_show as i64));
+        }
+        CmdType::Clone { ref args } => {
+            let data = get_cache_from_disk().unwrap();
+            clone(args, &data);
+        }
+        CmdType::Show => {
+            let data = get_cache_from_disk().unwrap();
+            print_recent(&data, Duration::days(args.days_to_show as i64));
             println!("{data:#?}");
         }
-        CmdType::Clone { ref url } => {
-            clone(url);
-        }
-        CmdType::Show => {}
     }
 
-    println!("{args:?}");
+    //println!("{args:?}");
 }
